175685fb0271ecee7a0ac4753e87550e344c2183
NicolasCornaglia <nicolascornaglia0@gmail.com>
2023-03-14 17:08:11 -0300

    working on fix for home video not autoplaying on safari

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -1,14 +1,36 @@
 import './index.scss'
 import danceVideo from '../../assets/videos/Video-BAILE.MP4'
+import React, { useEffect, useRef } from "react";
 
 const BackgroundVideo = () => {
+   const videoEl = useRef(null);
 
-   document.getElementById('vid').play();
+   const attemptPlay = () => {
+      videoEl &&
+         videoEl.current &&
+         videoEl.current.play().catch(error => {
+            console.error("Error attempting to play", error);
+         });
+   };
+
+   useEffect(() => {
+      attemptPlay();
+   }, []);
 
    return (
       <>
          <div className='video-container'>
-            <video className='video' id="background-video vid" src={danceVideo} type="video/mp4" autoPlay loop muted />
+            <video
+               className='video'
+               id="background-video"
+               playsInline
+               loop
+               muted
+               controls
+               alt="All the devices"
+               src={danceVideo}
+               ref={videoEl}
+            />
          </div>
       </>
    )
