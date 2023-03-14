567df9457e5c7753650d4b28a56a1260f988eba9
NicolasCornaglia <nicolascornaglia0@gmail.com>
2023-03-14 16:38:14 -0300

    fixing autoplay in safari with allowsInlineMediaPlayback  atribute on video html tag

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -5,7 +5,7 @@ const BackgroundVideo = () => {
    return (
       <>
          <div className='video-container'>
-            <video id="background-video" src={danceVideo} type="video/mp4" autoPlay loop muted playsInline className='video' />
+            <video className='video' id="background-video" src={danceVideo} type="video/mp4" autoPlay loop muted playsInline allowsInlineMediaPlayback  />
          </div>
       </>
    )
