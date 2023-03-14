462c9baa480a89e83548033c2ae71e5afc03be1f
NicolasCornaglia <nicolascornaglia0@gmail.com>
2023-03-14 16:56:48 -0300

    working on fix for home video not autoplaying on safari

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -2,10 +2,13 @@ import './index.scss'
 import danceVideo from '../../assets/videos/Video-BAILE.MP4'
 
 const BackgroundVideo = () => {
+
+   document.getElementById('vid').play();
+
    return (
       <>
          <div className='video-container'>
-            <video className='video' id="background-video" src={danceVideo} type="video/mp4" autoPlay loop muted playsInline allowsInlineMediaPlayback webkit-playsinline="true" webkit-playsInline="true"/>
+            <video className='video' id="background-video vid" src={danceVideo} type="video/mp4" autoPlay loop muted />
          </div>
       </>
    )
